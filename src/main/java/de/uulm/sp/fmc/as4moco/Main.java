7ec761122c15aa826d5f14975796d3cb323b0388
RSD6170 <rsd6170+github@mailbox.org>
2023-10-24 06:00:06 +0000

    feat: add simple main

--- src/main/java/de/uulm/sp/fmc/as4moco/Main.java
+++ src/main/java/de/uulm/sp/fmc/as4moco/Main.java
@@ -1,5 +1,6 @@
 package de.uulm.sp.fmc.as4moco;
 
+import org.apache.commons.cli.*;
 import org.collection.fm.FeatureStepAnalysis;
 import org.collection.fm.handler.AnalysisStepHandler;
 import org.collection.fm.handler.FeatureStep;
@@ -14,17 +15,44 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        FMUtils.installLibraries();
-        AnalysisStepHandler analysisStepHandler = new AnalysisStepHandler();
-        EnumMap<AnalysisStepsEnum, Integer> enumMap = new EnumMap<>(AnalysisStepsEnum.class);
+        CommandLine commandLine = parseCommandLine(args, generateOptions());
 
-        Arrays.stream(AnalysisStepsEnum.values()).forEach(e -> enumMap.put(e, 60));
+        WorkflowManager workflowManager = new WorkflowManager(commandLine.getOptionValue("model"));
 
-        analysisStepHandler.initializeHandler(enumMap);
+        workflowManager.runSolving(commandLine.getOptionValue("cnf"));
 
-        List<FeatureStep> list = analysisStepHandler.getSingleAnalysis(new File("src/main/resources/test.dimacs"));
-        list.forEach(System.out::println);
-        
     }
+
+    private static CommandLine parseCommandLine(String[] args, Options options){
+        CommandLineParser commandLineParser = new DefaultParser(false);
+        try{
+            return commandLineParser.parse(options, args);
+
+        } catch (ParseException e) {
+            System.err.println("Unexpected exception:" + e.getMessage());
+            printHelp(options);
+            System.exit(-1);
+        }
+        return null;
+    }
+
+    private static Options generateOptions(){
+        Options options = new Options();
+        OptionGroup mainOptions = new OptionGroup();
+        mainOptions.setRequired(true);
+        //TODO enbable more functionality, e.g. model training, feature extraction, ...
+        mainOptions.addOption(Option.builder("modelFile").argName("model").hasArg().desc("Path to trained autofolio model").build());
+        mainOptions.addOption(Option.builder("cnfFile").argName("cnf").hasArg().desc("Path to cnf").build());
+
+
+        options.addOptionGroup(mainOptions);
+
+        return options;
+    }
+
+    private static void printHelp(Options options){
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp("as4moco", "A simple algorithm selection #SAT solver!", options, "For more information, see associated paper: \n link", true); //TODO fix paper link
+    }
+
 }
