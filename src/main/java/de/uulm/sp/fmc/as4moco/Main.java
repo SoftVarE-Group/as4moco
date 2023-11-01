ffb3ff0a058f48b19d53d3866282813376091821
RSD6170 <rsd6170+github@mailbox.org>
2023-11-01 20:20:04 +0000

    feat: add helper methods

--- src/main/java/de/uulm/sp/fmc/as4moco/Main.java
+++ src/main/java/de/uulm/sp/fmc/as4moco/Main.java
@@ -3,24 +3,18 @@ package de.uulm.sp.fmc.as4moco;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import de.uulm.sp.fmc.as4moco.selection.messages.SolverBudget;
 import de.uulm.sp.fmc.as4moco.solver.SolverHandler;
-import de.uulm.sp.fmc.as4moco.solver.SolverInterface;
 import de.uulm.sp.fmc.as4moco.solver.SolverResponse;
 import de.uulm.sp.fmc.as4moco.solver.SolverStatusEnum;
 import org.apache.commons.cli.*;
-import org.collection.fm.FeatureStepAnalysis;
-import org.collection.fm.handler.AnalysisStepHandler;
-import org.collection.fm.handler.FeatureStep;
-import org.collection.fm.util.AnalysisStepsEnum;
-import org.collection.fm.util.FMUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
@@ -35,6 +29,13 @@ public class Main {
 
     }
 
+    /**
+     * Starts single normal solver run
+     * @param commandLine
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     private static void runNormal(CommandLine commandLine) throws IOException, ExecutionException, InterruptedException {
         Instant before = Instant.now();
 
@@ -47,7 +48,33 @@ public class Main {
         System.out.println("Duration: " + Duration.between(before, after).toMillis()/1000f + "s");
     }
 
-    private static List<SolvingRun> runMultiple(List<File> cnfs, File modelFile, File saveFile) throws IOException, ExecutionException, InterruptedException {
+    /**
+     * Generates CNF-Filenames from CSV-Split (Helper for runNormalMultiple)
+     * @param csvFile csv file with split
+     * @param split split to generate
+     * @param cnfFolder folder with cnf files
+     * @return list of cnf files
+     * @throws IOException
+     */
+    private static List<File> generateFileNames(File csvFile, int split, File cnfFolder) throws IOException {
+        try (CSVParser parser = CSVParser.parse(csvFile, Charset.defaultCharset(), CSVFormat.Builder.create(CSVFormat.DEFAULT).setHeader().setAllowMissingColumnNames(true).setSkipHeaderRecord(true).build())) {
+            return parser.stream().filter(e -> Integer.parseInt(e.get("Split")) == split)
+                    .map(e -> "mc2022_track1_%03d.dimacs".formatted(Integer.parseInt(e.get("InstanceNo"))))
+                    .map(e -> new File(cnfFolder.getAbsolutePath() + File.separator + e)).toList();
+        }
+    }
+
+    /**
+     * Runs multiple normal solver runs in batch mode
+     * @param cnfs cnfs to analyze
+     * @param modelFile model file to use for autofolio
+     * @param saveFile file to save to
+     * @return list of runs
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static List<SolvingRun> runNormalMultiple(List<File> cnfs, File modelFile, File saveFile) throws IOException, ExecutionException, InterruptedException {
         List<SolvingRun> solvingRuns = new ArrayList<>(cnfs.size());
 
         JsonFactory factory = new JsonFactory();
@@ -57,6 +84,7 @@ public class Main {
              JsonGenerator out = factory.createGenerator(saveFile, JsonEncoding.UTF8)) {
             out.writeStartArray();
             for (File cnf : cnfs) {
+                System.out.println("Run file "+cnf);
                 Instant before = Instant.now();
                 SolverResponse solverResponse;
                 try {
@@ -79,6 +107,36 @@ public class Main {
 
     }
 
+    /**
+     * Runs analysis of SBS and Oracles from CSV file
+     * @param csvFile csv file with data
+     * @param sbsOut file to save sbs runs to
+     * @param oracleOut file to save oracle runs to
+     * @param timeout timeout for solver
+     * @throws IOException
+     */
+    private static void runSBSOracleAnalysis(File csvFile, File sbsOut, File oracleOut, int timeout) throws IOException {
+        try (CSVParser parser = CSVParser.parse(csvFile, Charset.defaultCharset(), CSVFormat.Builder.create(CSVFormat.DEFAULT).setHeader().setAllowMissingColumnNames(true).setSkipHeaderRecord(true).build())) {
+            List<RunTask> sbsList = new ArrayList<>();
+            List<RunTask> oracleList = new ArrayList<>();
+            parser.stream().forEach(e -> {
+                File instance = new File(e.get("InstanceNo"));
+                sbsList.add(new RunTask(e.get("SBS"), instance, timeout));
+                oracleList.add(new RunTask(e.get("Oracle"), instance, timeout));
+            });
+            testSolver(sbsList, sbsOut);
+            testSolver(oracleList, oracleOut);
+        }
+    }
+
+
+    /**
+     * Runs daughter solvers
+     * @param tasks list of tasks for daughter solvers
+     * @param saveFile file to save to
+     * @return list of runs
+     * @throws IOException
+     */
     private static List<SolvingRun> testSolver(List<RunTask> tasks, File saveFile) throws IOException{
         List<SolvingRun> solvingRuns = new ArrayList<>(tasks.size());
 
@@ -88,6 +146,7 @@ public class Main {
         try (JsonGenerator out = factory.createGenerator(saveFile, JsonEncoding.UTF8)) {
             out.writeStartArray();
             for (RunTask task : tasks) {
+                System.out.println("Run task "+task);
                 Instant before = Instant.now();
                 SolverResponse solverResponse;
                 try {
